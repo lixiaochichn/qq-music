f2c970ee9347ef64840df38ca8cdc3bf1a1a07ae
lixiaochichn@gmail.com <lixiaochichn@gmail.com>
2017-08-29 23:59:57 +0800

    updata0829

--- scripts/app.js
+++ scripts/app.js
@@ -9,8 +9,9 @@ document.addEventListener('DOMContentLoaded', function () {
         constructor(options = {}) {
             this.$el = options.$el
             this.sliders = options.sliders
-            this.interval = options.interval || 3000
+            this.interval = options.interval || 2000
             this.duration = options.duration || 300
+            // this.restart = 0
             this.index = 0
             this.render()
             this.start()
@@ -20,10 +21,10 @@ document.addEventListener('DOMContentLoaded', function () {
             this.$el.innerHTML = '<div class="qq-slider-wrap"></div>'
             this.$wrap = this.$el.firstElementChild
             this.$wrap.style.transitionDuration = `${this.duration}ms`
-            this.$wrap.style.width = `${this.sliders.length * 100}%`
-            this.$wrap.innerHTML = this.sliders.map(slider => 
+            this.$wrap.style.width = `${this.sliders.length * 100 +100}%`
+            this.$wrap.innerHTML = this.sliders.map(slider =>
                 `<div class="qq-slider-item"><a href=${slider.url}><img src="${slider.img}"></a></div>`
-            ).join('')
+            ).join('') + `<div class="qq-slider-item"><a href=${this.sliders[0].url}><img src="${this.sliders[0].img}"></a></div>`
         }
 
         start() {
@@ -33,11 +34,18 @@ document.addEventListener('DOMContentLoaded', function () {
         next() {
             this.index += 1;
             if (this.index === this.sliders.length) {
-                this.$wrap.style.transform = `translate(0)`
+                this.$wrap.style.transform = `translate(-${this.index/(this.sliders.length+1)*100}%)`;
+                setTimeout(this.restart.bind(this), this.duration);
                 this.index = 0
                 return
             };
-            this.$wrap.style.transform = `translate(-${this.index/this.sliders.length*100}%)`;
+            this.$wrap.style.transitionDuration = `${this.duration}ms`
+            this.$wrap.style.transform = `translate(-${this.index/(this.sliders.length+1)*100}%)`;
+        }
+
+        restart() {
+            this.$wrap.style.transitionDuration = `0ms`
+            this.$wrap.style.transform = `translate(0)`
         }
 
     };
@@ -68,7 +76,7 @@ document.addEventListener('DOMContentLoaded', function () {
         ]
     });
 
-    window.slider = slider;
+
 
 
 
