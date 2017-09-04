b4be3b0516235986df0a89c3a67d65b91357c543
lixiaochichn@gmail.com <lixiaochichn@gmail.com>
2017-09-05 00:59:55 +0800

    updata0905

--- scripts/seek.js
+++ scripts/seek.js
@@ -7,9 +7,12 @@ document.addEventListener('DOMContentLoaded', function () {
     $hotsearch = document.querySelector('.hot-search'); //热门搜索
     $historylists = document.querySelector('.history-lists'); //搜索历史
     $icondel = document.querySelector('.icon-del'); //圆形叉号
+    $searchresultlist = document.querySelector('.search-result-list'); //searchresultlist
+    let keyword;
+    let page;
 
     // fetch('/json/history.json')
-    fetch('https://qq-music-api.now.sh/hotkey')
+        fetch('https://qq-music-api.now.sh/hotkey')
         .then(res => res.json())
         .then(json => json.data)
         .then(renderthird)
@@ -17,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function renderthird(data) {
         renderseek(data);
-        console.log(data);
+        // console.log(data);
 
         // lazyload();
     };
@@ -51,11 +54,48 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
     $inputseek.addEventListener("keyup", function () {
+        let which = event.which;
         if ($inputseek.value.length > 0) {
-            $icondel.classList.remove('hide');
-            return;
+            $icondel.classList.remove('hide'); //显示圆形删除符号
+        } else {
+            $icondel.classList.add('hide'); //隐藏圆形删除符号
         };
-        $icondel.classList.add('hide');
+        if ($inputseek.value.length > 0 && which === 13) {
+            console.log("which");
+            $historylists.classList.add('hide');
+            keyword = $inputseek.value;
+            page = 1;
+            search(keyword);
+        };
+    });
+
+    function search(keywordvalue) {
+        fetch(`https://qq-music-api.now.sh/search?keyword=${keywordvalue}?page=${page}`)
+            .then(res => res.json())
+            .then(json => json.data)
+            .then(renderforth)
+    }
+
+    function renderforth(data) {
+        rendersearch(data.song.list);
+    };
+
+    function rendersearch(lists) {
+        $searchresultlist.innerHTML += lists.map(list => `
+        <li class="search-result">
+        <i class="icon music-icon"></i>
+        <h6 class="result-title">${list.songname}</h6>
+        <p class="result-singer">${list.singer[0].name}</p>
+        </li>
+        `).join('');
+        page++;
+    };
+
+    window.addEventListener('scroll', function () {
+        console.log(pageYOffset);
+        if (pageYOffset + document.documentElement.clientHeight > document.body.scrollHeight - 50) {
+            search(keyword);
+        }
     });
 
 
